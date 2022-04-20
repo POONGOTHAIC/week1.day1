e3d33033d6d19e564ad010296c0622baccff5edd
POONGOTHAIC <104100992+POONGOTHAIC@users.noreply.github.com>
2022-04-21 01:33:21 +0530

    Add files via upload
    week1.day1.assignments

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+package week1.day1;
+
+public class Factorial {
+	public static void main(String[] args) {
+		int s = 5, fact = 1;
+		for(int i = 1; i <= s; i++) 
+			fact = fact * i;
+		System.out.println(fact);
+		
+		
+	}	
+
+}
